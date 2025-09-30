6c8b1cfcdc35761226b4272d88823acd9990b21c
Henrique Lopes Fouquet <henriquefouquet@gmail.com>
2025-09-30 19:35:08 -0300

    feat: Random enemies and items placed on the level

--- area.gd
+++ area.gd
@@ -3,9 +3,23 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_up()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func set_up() -> void:
+	var lines = 4
+	var collumns = 4
+	var square_size = %TextureRect.size.x
+	var margin = square_size / 10
+	var gap = (square_size - 2*margin) / lines
+	
+	for line in range(lines):
+		for collumn in range(collumns):
+			var marker = Marker2D.new()
+			marker.position = Vector2(line*gap + margin, collumn*gap + margin)
+			%SpawnPoints.add_child(marker)
+	
--- level.gd
+++ level.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
-var Quadrado = preload("res://area.tscn")
+var QuadradoScene = preload("res://area.tscn")
+var EnemyScene = preload("res://enemy.tscn")
+var HealthItemScene = preload("res://life_collectable.tscn")
+var BombItemScene = preload("res://bomb_collectable.tscn")
+
+@export var items_per_area = 3
+@export var enemies_per_area = 4
 
 @onready var player = %Player
 @onready var hud = %Hud
@@ -17,6 +23,31 @@ func set_up(linhas : int, colunas : int):
 	var size = 700
 	for x in range(linhas):
 		for y in range(colunas):
-			var q = Quadrado.instantiate()
+			var q = QuadradoScene.instantiate()
 			q.position = Vector2(x*size, y*size)
 			$Grid.add_child(q)
+			
+			var spawn_points = []
+			for marker in q.get_node("SpawnPoints").get_children():
+				var global_pos = q.position + marker.position
+				spawn_points.append(global_pos)
+			
+			for i in range(enemies_per_area):
+				if spawn_points.is_empty():
+					break
+				var pos = spawn_points.pick_random()
+				spawn_points.erase(pos)
+				var enemy = EnemyScene.instantiate()
+				enemy.position = pos
+				%Enemies.add_child(enemy)
+			
+			for i in range(items_per_area):
+				if spawn_points.is_empty():
+					break
+				var pos = spawn_points.pick_random()
+				spawn_points.erase(pos)
+				var scene = [HealthItemScene, BombItemScene].pick_random()
+				var item = scene.instantiate()
+				item.position = pos
+				%Collectables.add_child(item)
+				
